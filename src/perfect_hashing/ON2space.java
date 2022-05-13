94023354ef8c0943e519ccab6aede475197e3c70
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-05-14 01:26:59 +0200

    Add files via upload

---
+++ src/perfect_hashing/ON2space.java
@@ -0,0 +1,54 @@
+package perfect_hashing;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ON2space {
+	hash hashFun;
+    Integer[] hashTable;
+ 
+    public void hash(Integer[] keys) {
+        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(keys)); //convert keys array into non-repeated set
+        Integer[] uniqueKeys = new Integer[set.size()];
+        Iterator<Integer> iterator = set.iterator();
+        int index = 0;
+        //put the non-repeated set elements back to a new array
+        while (iterator.hasNext()) {
+            uniqueKeys[index++] = iterator.next();
+        }
+        int hashCounter = 0;
+        boolean hasCollision = true;
+        while (hasCollision) {
+            hashCounter++;
+            hasCollision = false;
+            hashTable = new Integer[uniqueKeys.length * uniqueKeys.length];
+            hashFun = new hash(uniqueKeys.length * uniqueKeys.length); //table size
+            for (int i = 0; i < uniqueKeys.length; i++) {
+                int s = hashFun.hash(uniqueKeys[i]); //result of multiplying h*x
+                if(hashTable[s] != null) {
+                    hasCollision = true;
+                    break;
+                }
+                else
+                    hashTable[s] = new Integer(uniqueKeys[i]);
+            }
+        }
+        System.out.println("Hashed in "+hashCounter+" times.");
+    }
+
+    public boolean contains(Integer key) {
+        if(hashFun == null)
+            return false;
+        int s = hashFun.hash(key);
+        if(hashTable[s] == null)
+            return false;
+        if(hashTable[s].equals(key))
+            return true;
+        else
+            return false;
+    }
+    
+}
